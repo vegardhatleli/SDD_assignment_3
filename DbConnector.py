05338dc6ceaf4d26b0a371a86024b90bdbdfa782
vegardhatleli <vegard.hatleli@gmail.com>
2024-10-15 13:13:24 +0200

    added password and connection

--- DbConnector.py
+++ DbConnector.py
@@ -13,10 +13,10 @@ class DbConnector:
     """
 
     def __init__(self,
-                 DATABASE='DATABASE_NAME',
-                 HOST="tdt4225-xx.idi.ntnu.no",
-                 USER="TEST_USER",
-                 PASSWORD="test123"):
+                 DATABASE='group13_ex3',
+                 HOST="tdt4225-13.idi.ntnu.no",
+                 USER="user13",
+                 PASSWORD="user13"):
         uri = "mongodb://%s:%s@%s/%s" % (USER, PASSWORD, HOST, DATABASE)
         # Connect to the databases
         try:
